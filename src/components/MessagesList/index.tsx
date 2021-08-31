4af4497dc30e7e740a550e925b2bba9bbf9ffa34
clairealmozinos <claire.almozinos@gmail.com>
2021-08-31 12:29:48 +0200

    feat(messages): manage redirection if no currentconversation

--- src/components/MessagesList/index.tsx
+++ src/components/MessagesList/index.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
 import { State } from 'src/store/reducers/index.types'
 import * as Actions from 'store/actions/index'
 
@@ -7,6 +8,14 @@ const MessagesList = ({ id }) => {
   const currentConversation = useSelector((state: State) => state.conversations[id])
   const messages = useSelector((state: State) => state.messages)
   const dispatch = useDispatch()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (currentConversation === undefined) {
+      router.replace('/')
+    }
+  }, [currentConversation])
+
 
   useEffect(() => {
     dispatch(Actions.messages.onMessagesFetch(id))
